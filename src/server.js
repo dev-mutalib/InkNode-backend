cbad7780c3e60191e281682030b5a5007e53b5ee
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-04 19:39:10 +0530

    feat: Call the database connection func in the root file.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-import app from "./app.js";
+import app from './app.js';
+import connectDB from './config/database.js';
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
-     console.log(`Server is running on PORT ${PORT}`)
-})
+connectDB(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
