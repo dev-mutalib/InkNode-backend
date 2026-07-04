9a310ae16de948a0b71434866a9c3e783c2185d3
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-04 21:19:56 +0530

    feat: create User schema using mongoose

---
+++ src/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      toLowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email'],
+    },
+    password: {
+      type: String,
+      required: true,
+      min: [6, 'Password must be at least 6 characters long'],
+      select: false,
+    },
+    role: {
+      type: String,
+      default: 'user',
+      enum: ['user', 'admin'],
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    bio: {
+      type: String,
+      default: '',
+      maxLength: [500, 'Bio cannot execced more 500 characters'],
+    },
+    socailLinks: {
+      website: { type: String, default: '' },
+      twitter: { type: String, default: '' },
+      github: { type: String, default: '' },
+      linkedin: { type: String, default: '' },
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
+  },
+  { timestamps: true },
+);
+
+//Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+//compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
