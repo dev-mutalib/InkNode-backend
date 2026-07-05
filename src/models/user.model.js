7fe05609ffb829dddcbeb33ac49113c4d60110e1
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-06 00:19:24 +0530

    feat(auth-model): change the avatar --> profile in userSchema.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
       default: 'user',
       enum: ['user', 'admin'],
     },
-    avatar: {
+    profile: {
       type: String,
       default: '',
     },
