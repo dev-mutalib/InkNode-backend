8e71ea4437217bf93ede7838c5d7cccd274cfc4f
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-06 19:30:05 +0530

    feat(auth.middleware): make the middleware both mobile App and API clients support

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,9 +5,11 @@ async function protect(req, res, next) {
   try {
     let token = req.cookies?.accessToken;
     if (
+      !token &&
       req.headers.authorization &&
       req.headers.authorization.startsWith('Bearer')
     ) {
+      token = req.headers.authorization.split(' ')[1];
     }
     if (!token) {
       return res.status(401).json({
