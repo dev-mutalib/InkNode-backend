6706170e026a2158195c5a7e1faac110b0ccdf87
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-06 22:59:32 +0530

    feat(admin): add middleware to restrict access to admin users

---
+++ src/middleware/admin.middleware.js
@@ -0,0 +1,11 @@
+function admin(req, res, next) {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  }
+  return res.status(403).json({
+    success: false,
+    message: 'Access denied. Admin privilages requires ',
+  });
+}
+
+export default admin;
