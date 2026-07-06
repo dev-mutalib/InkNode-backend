28a5142681c788443f85e2cb8cf389fe97bfa554
Abdul Mutalib <mutalib92847@gmail.com>
2026-07-06 20:55:48 +0530

    refactor(app): organize imports and improve code formatting

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-import express, { urlencoded } from "express";
-import 'dotenv/config'
-import cors from 'cors'
-import cookieParser from "cookie-parser";
-import helmet from "helmet";
-import morgan from "morgan";
+import express, { urlencoded } from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan('dev'));
 
-app.use(express.json())
-app.use(urlencoded({extended: true}))
-app.use(cors())
-app.use(cookieParser())
-app.use(helmet())
-app.use(morgan('dev'))
-
-
-app.get('/', (req, res)=>{
-     res.send('API is running🚀')
-})
+app.get('/', (req, res) => {
+  res.send('API is running🚀');
+});
 
 // Routes
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
-export default app
+export default app;
